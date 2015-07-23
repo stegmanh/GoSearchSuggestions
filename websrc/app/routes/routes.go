b88d63387b0a9bdffd176ba3c66d4642841ee46b
Holden <stegman.holden@gmail.com>
2015-07-22 21:37:55 -0700

    Started connecting routes together

--- websrc/app/routes/routes.go
+++ websrc/app/routes/routes.go
@@ -11,6 +11,7 @@ func InitHandler() http.Handler {
 	router := mux.NewRouter()
 	router.HandleFunc("/", Home)
 	router.HandleFunc("/dashboard", Dashboard)
+	router.HandleFunc("/articles", DbSearchHandler)
 	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./websrc/static/"))))
 	return router
 }
