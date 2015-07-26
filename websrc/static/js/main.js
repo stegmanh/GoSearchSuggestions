295520061729d652ba80e05101c56cfd4877fea1
Holden <stegman.holden@gmail.com>
2015-07-25 22:53:00 -0700

    Switched to vue.js instead of angular

--- websrc/static/js/main.js
+++ websrc/static/js/main.js
@@ -1,19 +1,26 @@
 "use strict";
 
-angular.module('SearchApp', [])
-	.controller('SearchController', function($scope, $http) {
-		$scope.searchTerm
-		$scope.suggestions = []
-		$scope.articles = []
+var demo = new Vue({
+    el: '#app',
+    data: {
+        term: null,
+        results: null
+    },
 
-		$scope.search = function(term) {
-			$http.get('/autocomplete?q=' + term).
-			  success(function(data, status, headers, config) {
-			  	console.log(data)
-			  }).
-			  error(function(data, status, headers, config) {
-			    // called asynchronously if an error occurs
-			    // or server returns response with an error status.
-			  });
-		}
-	});
+    methods: {
+    	autoComplete: function() {
+			var self = this
+			if (self.term.length < 1) {
+				self.results = null
+				return
+			}
+    		var xhr = new XMLHttpRequest()
+	      xhr.open('GET', "/autocomplete?q=" + self.term)
+	      xhr.onload = function () {
+	      		console.log(JSON.parse(xhr.responseText))
+        		self.results = JSON.parse(xhr.responseText)
+	      }
+	      xhr.send()
+    	}
+    }
+})
