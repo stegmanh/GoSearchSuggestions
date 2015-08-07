8e4cfcee360b2bf7a20891bcb2f6fc5c1b5451aa
Holden <stegman.holden@gmail.com>
2015-08-06 21:31:58 -0700

    Improve authentication support minorly

--- websrc/app/auth/auth.go
+++ websrc/app/auth/auth.go
@@ -1,8 +1,10 @@
 package auth
 
 import (
+	"encoding/json"
 	"errors"
-	"fmt"
+	//	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	ctx "github.com/gorilla/context"
@@ -15,15 +17,38 @@ var Store = sessions.NewCookieStore(
 	[]byte(securecookie.GenerateRandomKey(64)), //Signing key
 	[]byte(securecookie.GenerateRandomKey(32)))
 
+var credentials auth
+
+type auth struct {
+	User     string
+	Password []byte
+}
+
 func Login(r *http.Request) (bool, error) {
 	err := errors.New("Error")
+	username := r.FormValue("username")
 	password := r.FormValue("password")
 	session, _ := Store.Get(r, "admin")
-	fmt.Println(bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost))
-	if password != "wow" {
+	err = bcrypt.CompareHashAndPassword([]byte(password), credentials.Password)
+	if err != nil {
 		return false, err
 	}
+	if username != credentials.User {
+		return false, errors.New("Wrong Username")
+	}
+	//This is the serialized information from username if we were using a real model
 	ctx.Set(r, "user", "holden")
 	session.Values["id"] = 123
 	return true, nil
+}
+
+func Init() {
+	creds, err := ioutil.ReadFile("./websrc/cred.json")
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(creds, &credentials)
+	if err != nil {
+		panic(err)
+	}
 }
